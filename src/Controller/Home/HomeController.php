7c751cd5468dd1629d5ee57f9c513e1c739f2cfe
dhiabenmaati <d.benmaati@gmail.com>
2022-02-28 14:35:11 +0100

    Initial commit

---
+++ src/Controller/Home/HomeController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller\Home;
+
+use App\Entity\Admin\Messages;
+use App\Entity\Admin\Product;
+use App\Form\Admin\MessagesType;
+use App\Repository\Admin\CommentRepository;
+use App\Repository\Admin\ImageRepository;
+use App\Repository\Admin\ProductRepository;
+use App\Repository\Admin\SettingRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index( SettingRepository $settingRepository)
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/contact-us", name="contact")
+     */
+    public function contact( SettingRepository $settingRepository)
+    {
+        return $this->render('home/contact.html.twig');
+    }
+
+    /**
+     * @Route("/about-us", name="about")
+     */
+    public function about( SettingRepository $settingRepository)
+    {
+        return $this->render('home/aboutus.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing( SettingRepository $settingRepository)
+    {
+        return $this->render('home/pricing.html.twig');
+    }
+
+
+}
